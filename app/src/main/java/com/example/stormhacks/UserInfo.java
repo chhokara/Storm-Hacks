42c329dbd7306e82e31f3dbff65fc597d6bb2aa8
Arsh <chhokara@gmail.com>
2021-02-20 16:44:24 -0800

    user info

---
+++ app/src/main/java/com/example/stormhacks/UserInfo.java
@@ -0,0 +1,19 @@
+package com.example.stormhacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+//        isFieldEntered();
+    }
+
+//    private boolean isFieldEntered() {
+//
+//    }
+}
